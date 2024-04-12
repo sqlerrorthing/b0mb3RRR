382d063232525ff035a2ce5cdcc92a4b175ba52d
oneqxz <oneqxz@gmail.com>
2024-04-12 19:35:18 +0800

    improved proxy file read

--- src/main/java/me/oneqxz/b0mb3rrr/data/proxy/Proxys.java
+++ src/main/java/me/oneqxz/b0mb3rrr/data/proxy/Proxys.java
@@ -5,6 +5,7 @@ import lombok.extern.log4j.Log4j2;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.LinkedList;
 import java.util.List;
@@ -16,21 +17,31 @@ public class Proxys {
     private final File file;
     private final Proxy[] proxys;
 
-    @SneakyThrows
     public Proxys(File file)
     {
         this.file = file;
         List<Proxy> _proxys = new LinkedList<>();
 
-        for(String line : FileUtils.readLines(file, StandardCharsets.UTF_8))
+        try
         {
-            String[] proxy = line.split(":");
-            _proxys.add(new Proxy.ProxyBuilder()
-                    .ip(proxy[0])
-                    .port(Integer.parseInt(proxy[1]))
-                    .type(java.net.Proxy.Type.HTTP)
-                    .build()
-            );
+            for(String line : FileUtils.readLines(file, StandardCharsets.UTF_8))
+            {
+                try
+                {
+                    String[] proxy = line.split(":");
+                    _proxys.add(new Proxy.ProxyBuilder()
+                            .ip(proxy[0])
+                            .port(Integer.parseInt(proxy[1]))
+                            .type(java.net.Proxy.Type.HTTP)
+                            .build()
+                    );
+                }
+                catch (Exception ignored) {}
+            }
+        }
+        catch (IOException e)
+        {
+            log.fatal("Не удалось прочитать файл с прокси", e);
         }
 
         if(_proxys.isEmpty())
